6fcd558f8494acdc4dd05281a929f7ac372ce821
Alan Emeršić <alan.emersic@gmail.com>
2022-02-05 22:51:13 +0100

    init

---
+++ backend/src/test/java/com/app/moneyheist/MoneyHeistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.moneyheist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyHeistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
